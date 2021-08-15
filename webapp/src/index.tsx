4b7bf586b0711ddf7ce36bfcad8e562d5a65d8b1
Hideyuki Kagami <hide212131@gmail.com>
2021-08-15 11:54:38 +0900

    test filesWillUploadHook

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -9,14 +9,22 @@ import {PluginRegistry} from './types/mattermost-webapp';
 
 export default class Plugin {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
-    public async initialize(registry: PluginRegistry, store: Store<GlobalState, Action<Record<string, unknown>>>) {
+    public async initialize(
+        registry: PluginRegistry,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        store: Store<GlobalState, Action<Record<string, unknown>>>,
+    ) {
         // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        registry.registerFilesWillUploadHook((files, upload) => {
+            return (files.length >= 2) ? {message: 'Must upload one by one.'} : {files};
+        });
     }
 }
 
 declare global {
     interface Window {
-        registerPlugin(id: string, plugin: Plugin): void
+        registerPlugin(id: string, plugin: Plugin): void;
     }
 }
 
